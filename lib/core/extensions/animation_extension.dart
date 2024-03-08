0bafa20590406ab40ec54c6a429b39ad401dada8
erkam-dev <erkamfb14@gmail.com>
2024-03-09 01:22:48 +0300

    feat: animations extension added

---
+++ lib/core/extensions/animation_extension.dart
@@ -0,0 +1,85 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+
+extension AnimationExtension on Widget {
+  Widget fadeThroughTransition({Duration? duration, bool? reverse}) =>
+      PageTransitionSwitcher(
+        duration: duration ?? const Duration(milliseconds: 300),
+        reverse: reverse ?? false,
+        transitionBuilder: (child, animation, secondaryAnimation) =>
+            FadeThroughTransition(
+          animation: animation,
+          secondaryAnimation: secondaryAnimation,
+          fillColor: Colors.transparent,
+          child: child,
+        ),
+        child: this,
+      );
+
+  Widget sharedAxisTransition({
+    Duration? duration,
+    bool? reverse,
+    SharedAxisTransitionType? transitionType,
+  }) =>
+      PageTransitionSwitcher(
+        duration: duration ?? const Duration(milliseconds: 300),
+        reverse: reverse ?? false,
+        transitionBuilder: (child, animation, secondaryAnimation) =>
+            SharedAxisTransition(
+          transitionType: transitionType ?? SharedAxisTransitionType.horizontal,
+          animation: animation,
+          secondaryAnimation: secondaryAnimation,
+          fillColor: Colors.transparent,
+          child: child,
+        ),
+        child: this,
+      );
+
+  Widget animatedSize({
+    Duration? duration,
+    Duration? reverseDuration,
+    Curve? curve,
+    Alignment? alignment,
+    Function()? onEnd,
+    Clip? clipBehavior,
+  }) =>
+      AnimatedSize(
+        alignment: alignment ?? Alignment.center,
+        onEnd: onEnd,
+        reverseDuration: reverseDuration ?? const Duration(milliseconds: 300),
+        duration: duration ?? const Duration(milliseconds: 300),
+        curve: curve ?? Curves.easeOutCubic,
+        clipBehavior: clipBehavior ?? Clip.hardEdge,
+        child: this,
+      );
+
+  Widget animatedScale({
+    required double scale,
+    Duration? duration,
+    Curve? curve,
+    Alignment? alignment,
+    Function()? onEnd,
+  }) =>
+      AnimatedScale(
+        scale: scale,
+        duration: duration ?? const Duration(milliseconds: 300),
+        curve: curve ?? Curves.easeOutCubic,
+        alignment: alignment ?? Alignment.center,
+        onEnd: onEnd,
+        child: this,
+      );
+
+  Widget animatedOpacity({
+    required double opacity,
+    Duration? duration,
+    Curve? curve,
+    Function()? onEnd,
+  }) =>
+      AnimatedOpacity(
+        opacity: opacity,
+        duration: duration ?? const Duration(milliseconds: 300),
+        curve: curve ?? Curves.easeOutCubic,
+        onEnd: onEnd,
+        child: this,
+      );
+}
